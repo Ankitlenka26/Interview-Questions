bf2afdc804601d473166b712e5fabb164af7a08d
Ankit Lenka <ankitlenka26@gmail.com>
2020-07-19 22:02:03 +0530

    longest common prefix

--- LongestCommonPrefix.cpp
+++ LongestCommonPrefix.cpp
@@ -1,11 +1,37 @@
 #include<bits/stdc++.h>
 using namespace std;
+
+string commonString(vector<string> v){
+    if(v.size()==0){
+        return "" ; 
+    }
+    string ans = v[0]; 
+    for(int i= 1; i<v.size() ; i++){
+        string temp= "" ; 
+        for(int j=0 ; j<min(v[i].size() , ans.size()) ; j++){
+            if(ans[j] == v[i].at(j)){
+                temp += ans[j]; 
+            }
+            else{
+                break ;
+            }
+        }
+
+        ans = temp ;
+    }
+
+    return ans; 
+}
 int main()
 {
-    int t ; 
-    cin >> t;
-    while(t--){
-        cout << "hello" << endl ;
-    }
-    return 0;
+    int n; 
+    cin >> n ; 
+    vector<string> v(n,""); 
+    for(int i=0 ;i<n ;i++){
+        cin >> v[i]; 
+    } 
+    string ans =  commonString(v); 
+    cout << ans << endl; 
+    return 0 ;
+
 }
